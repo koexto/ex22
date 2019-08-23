de9e72870869d0e0e0791f0763378d0350b2fb39
koexto <boxkoe@gmail.com>
2019-08-23 17:59:14 +0300

    change componentsimple

---
+++ components/manao/simplecomp.exam/.description.php
@@ -0,0 +1,10 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+$arComponentDescription = array(
+	"NAME" => GetMessage("T_SIMPLECOMP_MY_COMPONENT"),
+	"DESCRIPTION" => GetMessage("T_SIMPLECOMP_DESCRIPTION"),
+	"PATH" => array(
+		"ID" => "Exam2",
+		"NAME" => GetMessage("T_SIMPLECOMP_PATH_EXAM2"),
+	),
+);
+?>
