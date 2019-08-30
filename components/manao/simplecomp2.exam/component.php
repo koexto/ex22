dc0ac05bdb6784970781a92673a08d0abd55a4b5
koexto <boxkoe@gmail.com>
2019-08-30 18:34:40 +0300

    ex2-58

--- components/manao/simplecomp2.exam/component.php
+++ components/manao/simplecomp2.exam/component.php
@@ -3,6 +3,21 @@
 if (!CModule::IncludeModule("iblock"))
 	return;
 
+$this->AddIncludeAreaIcons(
+	array(
+		array(
+			'URL' => "javascript:alert('Это новая кнопка для ".$this->GetName()."');",
+			'SRC' => $this->GetPath().'/images/znak.gif',
+			'TITLE' => "Это новая кнопка"
+		),
+		array(
+			'URL' => "javascript:alert('Это ещё одна кнопка для ".$this->GetName()."');",
+			'SRC' => $this->GetPath().'/images/znak1.gif',
+			'TITLE' => "Это ещё одна кнопка"
+		),
+	)
+);
+
 if (isset($_GET["F"]))
 {
 	$arResult = $this->getResult();
