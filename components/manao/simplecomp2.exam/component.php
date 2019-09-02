acbd19fb75f60f926837c12fd5522a581a56aa19
koexto <boxkoe@gmail.com>
2019-09-02 14:13:31 +0300

    end ex2-100

--- components/manao/simplecomp2.exam/component.php
+++ components/manao/simplecomp2.exam/component.php
@@ -4,6 +4,16 @@ if (!CModule::IncludeModule("iblock"))
 	return;
 
 
+$this->AddIncludeAreaIcon(
+	array(
+		"ID" => "IB_admin",
+		"TITLE" => "ИБ в админке",
+		"URL" => "http://" . SITE_SERVER_NAME . "/bitrix/admin/iblock_element_admin.php?IBLOCK_ID=" . $arParams["ID_IBLOCK_PRODUCTION"] .
+				"&type=products&lang=ru&find_el_y=Y&clear_filter=Y&apply_filter=Y", //или javascript:MyJSFunction ()
+		"IN_PARAMS_MENU" => true, //показать в контекстном меню
+		//"IN_MENU" => true //показать в подменю компонента
+	)
+);
 
 if (isset($_GET["F"]))
 {
